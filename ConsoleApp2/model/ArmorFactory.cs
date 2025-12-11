825b58714e5ecf7af960e50b9f6f9cf823eb9ada
Олег Алимов <spamban.oleg@yandex.ru>
2025-12-11 11:27:00 +0300

    c

---
+++ ConsoleApp2/model/ArmorFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.model
+{
+    public class ArmorFactory
+    {
+        private RandomService randomService;
+
+        public ArmorFactory(RandomService randomService)
+        {
+            this.randomService = randomService;
+        }
+
+        public Armor CreateRandomArmor()
+        {
+            var armors = new[]
+            {
+            new Armor("Кожаная броня", 5),
+            new Armor("Кольчуга", 8),
+            new Armor("Латы", 12),
+            new Armor("Мантия мага", 6),
+            new Armor("Доспех воина", 10)
+        };
+            return randomService.Choose(armors);
+        }
+
+        public Armor CreateStarterArmor()
+        {
+            return new Armor("Простая одежда", 2);
+        }
+    }
+}
