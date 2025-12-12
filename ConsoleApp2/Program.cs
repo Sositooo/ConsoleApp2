475fdde3d9f9aaba29616dc504b6f31d6b5a9f75
Олег Алимов <spamban.oleg@yandex.ru>
2025-12-12 15:32:12 +0300

    Добавил вызов игры, закончил код

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ConsoleApp2;
 
-namespace ConsoleApp2
+namespace YourProjectName
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Game game = new Game();
+            game.Start();
         }
     }
 }
