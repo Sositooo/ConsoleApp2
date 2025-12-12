f1ef66979132d3da0abb14c71ab6eaf72e9b12f6
Олег Алимов <spamban.oleg@yandex.ru>
2025-12-12 12:51:21 +0300

    создал главное меню

---
+++ ConsoleApp2/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Core
+    {
+        public static PR8GORDOVEntities Context = new PR8GORDOVEntities();
+
+    }
+}
