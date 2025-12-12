fe93b9b689fbde24e2fb13901007118eeb3eed64
Олег Алимов <spamban.oleg@yandex.ru>
2025-12-12 15:07:39 +0300

    Сделал класс генерации оружия

---
+++ ConsoleApp2/model/WeaponFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp2;
+
+namespace ConsoleApp2
+{
+    public class WeaponFactory
+    {
+        private RandomService randomService;
+
+        public WeaponFactory(RandomService randomService)
+        {
+            this.randomService = randomService;
+        }
+
+        public Weapon CreateRandomWeapon()
+        {
+            var weapons = new[]
+            {
+            new Weapon("Кинжал", 8),
+            new Weapon("Меч", 12),
+            new Weapon("Топор", 15),
+            new Weapon("Посох", 10),
+            new Weapon("Двуручный меч", 18)
+        };
+            return randomService.Choose(weapons);
+        }
+
+        public Weapon CreateStarterWeapon()
+        {
+            return new Weapon("Кулаки", 5);
+        }
+    }
+}
